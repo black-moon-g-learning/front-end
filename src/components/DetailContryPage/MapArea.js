d50d8074193ff128cc08c4c13e57e96719e535ae
Nguyet <nguyet.ho23@student.passerellesnumeriques.org>
2023-02-05 11:17:14 +0700

    finish edit css for detail country page

--- src/components/DetailContryPage/MapArea.js
+++ src/components/DetailContryPage/MapArea.js
@@ -23,12 +23,15 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     fontSize: 24,
     lineHeight: 20,
+    width: '20%',
+    // textAlign:'center',
   },
   undertxt: {
     backgroundColor: '#5FAD41',
     width: '20%',
     height: 2,
     margin: 3,
+    marginBottom: 20,
   },
   maparea: {
     padding: 10,
@@ -38,5 +41,6 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 195,
     padding: 10,
+    marginTop: 20,
   },
 });
