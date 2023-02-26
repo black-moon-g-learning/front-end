b3f5277bc1baa3db6bc44335791a294257cf42fd
black <dennttinh@gmail.com>
2023-02-26 22:13:00 +0700

    finish the question page

--- src/screens/G-Game/DisplayQuestion.js
+++ src/screens/G-Game/DisplayQuestion.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 const DisplayQuestion = () => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Image
-          style={styles.title}
-          source={require('../../assets/images/G-GAME.png')}
-        />
+        <Image source={require('../../assets/images/G-GAME.png')} />
       </View>
       <View style={styles.Timer}>
         <View style={styles.countTime} />
@@ -18,12 +15,12 @@ const DisplayQuestion = () => {
         />
       </View>
       <View style={styles.questionContainer}>
-        {/* <View style={styles.characterCon}>
+        <View style={styles.characterCon}>
           <Image
             style={styles.character}
-            source={require('../../assets/images/easy.png')}
+            source={require('../../assets/images/nomarl.png')}
           />
-        </View> */}
+        </View>
         <View style={styles.countQues}>
           <Text style={styles.countTxt}>Question 1/15</Text>
         </View>
@@ -50,6 +47,9 @@ const DisplayQuestion = () => {
             <Text style={styles.answer}>98,186 million people</Text>
           </View>
         </View>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.txtBtn}>Next</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -59,7 +59,7 @@ export default DisplayQuestion;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
+    paddingHorizontal: 5,
     flex: 1,
     alignItems: 'center',
   },
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#5FAD41',
     borderRadius: 20,
     width: '90%',
-    marginTop: 20,
+    marginTop: 50,
     alignItems: 'center',
     position: 'relative',
   },
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
     width: '90%',
     height: 40,
     borderRadius: 10,
-    marginTop: 20,
+    marginTop: 60,
   },
   countTxt: {
     fontSize: 20,
@@ -100,16 +100,12 @@ const styles = StyleSheet.create({
   },
   ansContainer: {
     width: '90%',
-    marginBottom: 10,
   },
   ans: {
     backgroundColor: '#FFFFFF',
     flexDirection: 'row',
     justifyContent: 'flex-start',
-    borderTopLeftRadius: 100,
-    borderBottomLeftRadius: 100,
-    borderBottomRightRadius: 30,
-    borderTopRightRadius: 30,
+    borderRadius: 100,
     margin: 10,
   },
   option: {
@@ -137,13 +133,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginVertical: 10,
-    width: '100%',
+    marginVertical: 15,
+    width: '95%',
     backgroundColor: '#5FAD41',
     borderRadius: 20,
     borderColor: '#5FAD41',
     borderWidth: 1,
-    borderBottomWidth: 2,
+    borderBottomWidth: 1,
     paddingRight: 10,
   },
   countTime: {
@@ -155,5 +151,33 @@ const styles = StyleSheet.create({
   clock: {
     width: 27,
     height: 27,
+  },
+  characterCon: {
+    position: 'absolute',
+    top: -60,
+    backgroundColor: '#5FAD41',
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+  },
+  character: {
+    width: 87,
+    height: 112,
+  },
+  btn: {
+    width: 94,
+    height: 32,
+    borderRadius: 10,
+    backgroundColor: '#FFC845',
+    margin: 10,
+  },
+  txtBtn: {
+    fontSize: 16,
+    fontWeight: '500',
+    lineHeight: 20,
+    fontFamily: 'Poppins-Bold',
+    color: '#000000',
+    textAlign: 'center',
+    paddingTop: 5,
   },
 });
