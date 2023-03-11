a5dc0206a2c49d6cd90f6cdf3d9d466a6ae7d9ee
duongAKT <duong.a23@student.passerellesnumeriques.org>
2023-03-11 12:01:41 +0700

    finish create notification when user out app

--- App.js
+++ App.js
@@ -2,19 +2,19 @@ import {NavigationContainer} from '@react-navigation/native';
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import {QueryClient, QueryClientProvider} from 'react-query';
-import Navigation from './src/navigations';
-// import SplashScreen from 'react-native-splash-screen'
 import {
   NotificationListner,
   requestUserPermission,
-} from './src/utills/Pushnotification';
+} from './src/components/Login/LoginSocial';
+import Navigation from './src/navigations';
+// import SplashScreen from 'react-native-splash-screen'
 
 const queryClient = new QueryClient();
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
-    requestUserPermission();
     NotificationListner();
+    requestUserPermission();
   }, []);
 
   const config = {
