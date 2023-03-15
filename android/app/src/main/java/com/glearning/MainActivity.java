65334d45d2068dae125a00f7bedeac4adee146ac
duongAKT <duong.a23@student.passerellesnumeriques.org>
2023-03-15 14:48:20 +0700

    finish create icon app and fix bug name app

--- android/app/src/main/java/com/glearning/MainActivity.java
+++ android/app/src/main/java/com/glearning/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Glearning";
+    return "G-learning";
   }
   // @Override
   // protected void onCreate(Bundle savedInstanceState) {
