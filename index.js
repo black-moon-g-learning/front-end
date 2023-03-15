037f754890bbb70791b1ab3d67ae646ac9b2af15
duongAKT <duong.a23@student.passerellesnumeriques.org>
2023-03-15 14:02:26 +0700

    fix bug video watched

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import messaging from '@react-native-firebase/messaging';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+// await TrackPlayer.setupPlayer();
+// AppRegistry.registerComponent(...);
+// TrackPlayer.registerPlaybackService(() => require('./service'));
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
